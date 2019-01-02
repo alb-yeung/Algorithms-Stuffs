badf76223c1d71227bcd5db6ee549b19045d500c
alb-yeung <alb.yeung@gmail.com>
2019-01-02 13:17:11 -0500

    Will get back to this

---
+++ countRangeSum/countRangeSum.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    /*  I'm sure divide and conquer is the way to go for this one but I'm not sure how to do the merge part just yet */
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        
+    }
+};
