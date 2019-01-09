bea4a61210132a3b3236df22897b5426d9c8bc7d
alb-yeung <alb.yeung@gmail.com>
2019-01-09 13:45:45 -0500

    Update uniquePathsWithObstacles.cpp

--- uniquePathsII/uniquePathsWithObstacles.cpp
+++ uniquePathsII/uniquePathsWithObstacles.cpp
@@ -26,4 +26,33 @@ public:
         }
         return paths[n-1][m-1];
     }
+    
+    /* got it to run in 0ms by not using extra space */
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }else if (i > 0){
+                    if (j > 0){
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                    }else{
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                    }
+                    continue;
+                }else if (j > 0){
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1];
+                    continue;
+                }
+                obstacleGrid[0][0] = 1;
+            }
+        }
+        return obstacleGrid[n-1][m-1];
+    }
 };
